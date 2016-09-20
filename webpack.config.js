a0caec097cdb27c2601fa90411acc5df2dd34710
FeHHix <ifenix0@gmail.com>
2016-09-20 18:21:02 +0300

    Added babel 6 for support ES2015, Es7 and JSX.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [ //откула начинать сборку
     'webpack-hot-middleware/client', //добавить поддержку hot-reload
+    'babel-polyfill',
     './src/index'
   ],
   output: { //куда генерировать дистрибутив
@@ -16,5 +17,17 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(), //добавить поддержку hot-reload
     new webpack.NoErrorsPlugin()
-  ]
+  ],
+  module: {
+    loaders: [ //добавление babel-loader
+      {
+        loaders: ['babel-loader'],
+        include: [
+          path.resolve(__dirname, "src") //обработка файлов babel-loader'ом
+        ],
+        test: /\.js$/,
+        plugins: ['transform-runtime']
+      }
+    ]
+  }
 }

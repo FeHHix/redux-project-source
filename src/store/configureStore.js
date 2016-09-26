ac4962739bceb2a651fa4f478d5d16d642caae93
FeHHix <ifenix0@gmail.com>
2016-09-26 11:29:31 +0300

    Added middleware logger.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,13 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import rootReducer from '../reducers'
+import createLogger from 'redux-logger'
 
 export default function configureStore(initiialState) {
-  const store = createStore(rootReducer, initiialState)
+  const logger = createLogger()
+  const store = createStore(
+    rootReducer,
+    initiialState,
+    applyMiddleware(logger))
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
